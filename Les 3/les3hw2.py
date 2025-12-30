f86ed75ee39ed91b058adca90f68cc65e523ee5b
MC295 <manuela_carrein@telenet.be>
2025-12-31 00:25:00 +0100

    Opruimen: lesbestanden verwijderd, alleen ijssalon-start behouden

--- Les 3/les3hw2.py
+++
@@ -1,2 +0,0 @@
-txt="Hallo Europa" #deze regel declareert een variabele genaamd 'txt' en kent de waarde 'Hallo Europa' toe  aan die variabele 
-print(txt) #deze regel print de waarde van variabele 'txt' op het scherm

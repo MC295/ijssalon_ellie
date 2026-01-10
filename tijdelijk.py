aebb5132f56e7ec38c379b5cb1016f3eda06a199
MC295 <manuela_carrein@telenet.be>
2026-01-10 22:21:56 +0100

    functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,35 +1,26 @@
-#2. dictionary met waarden voor aardbei 3, vanille 4 en chocolade 5
-prijzen = {
-    "aardbei": 3,
-    "vanille": 4,
-    "chocolade": 5
-}
-# 3. Bereken de aanbieding (aardbei vermenigvuldigd met 0.8)
-aanbieding = prijzen["aardbei"] * 0.8
+from helper import decoreer
 
-#4. reclametekst met een formatted string
-reclame_tekst= f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
+def print_aanbieding():
+    prijzen = {
+        "aardbei": 3,
+        "vanille": 4,
+        "chocolade": 5
+    }
+   
+    aanbieding = prijzen["aardbei"] * 0.8
 
-#5 printopdracht van punt 4 verwijderd
-komma_index= reclame_tekst.index(",") #5. index van de komma zoeken
-nul_index= reclame_tekst.index("0",komma_index) #5. index van de eerste 0 na de komma zoeken
-reclame_tekst2=reclame_tekst[:nul_index+1] #5. deel van de string nemen tem eerste 0 na de komma
+    reclame_tekst= f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
 
-#print uit stap 5 gewist
-# 6 zet de  tekst uit reclame_tekst 2 om in hoofdletters
-reclame_tekst3=reclame_tekst2.upper()
+    reclame_tekst2=reclame_tekst[:63]
 
-#7 print uit stap 6 wissen
-#7 de tekst uit reclame_tekst3 splitsen
-reclame_tekst4=reclame_tekst3.split(" ")
+    reclame_tekst3=reclame_tekst2.upper()
 
-#8 print opdracht uit 7 gewist
-#9 for loop van de tekst in reclame_tekst4 +printen
+    reclame_tekst4=reclame_tekst3.split(" ")
 
-#10 for loop van punt 9 aanpassen met een if loop en 
-# naargelang de lengte van el upper of lower case toepassen
-for el in reclame_tekst4:
-    if len(el)>=5:
-        print(el.upper())
-    else: 
-        print(el.lower())
+    for el in reclame_tekst4:
+        if len(el)>=5:
+            print(el.upper())
+        else: 
+            print(el.lower())
+decoreer("Aanbieding")
+print_aanbieding()

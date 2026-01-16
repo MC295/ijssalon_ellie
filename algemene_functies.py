d90a1bbdbb3dac1f8a50e633c40f6d48d905e606
MC295 <manuela_carrein@telenet.be>
2026-01-16 20:44:23 +0100

    Test mijn_functie_1 met waarden uit tabel

---
+++ algemene_functies.py
@@ -0,0 +1,9 @@
+#functie die waarden uit tabel teruggeeft maal zichzelf
+def mijn_functie_1(a):
+    return a*a
+
+#test met waarden uit de tabel
+print (mijn_functie_1(2))
+print (mijn_functie_1(4))
+print (mijn_functie_1(10))
+print (mijn_functie_1(12))

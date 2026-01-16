9661a0e0d6966fb557e7a802f71edfcf35b37337
MC295 <manuela_carrein@telenet.be>
2026-01-16 21:40:52 +0100

    Test print statements verwijderd

--- algemene_functies.py
+++ algemene_functies.py
@@ -6,8 +6,4 @@ def mijn_functie_2(a,b):
     my_list=[a+b, a-b, a*b, a/b] 
     return my_list
 
-#testing mijn_functie_2 met alle waarden uit de tabel
-print (mijn_functie_2(12,3))
-print (mijn_functie_2(12,2))
-print (mijn_functie_2(10,5))
-print (mijn_functie_2(100,20))
+

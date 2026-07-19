1fa0faaa8d9e483dab753525e50a10594c0c83fe
MC295 <manuela_carrein@telenet.be>
2026-07-19 11:21:56 +0200

    functie presenteer + = regel boven totaal

--- presentatie.py
+++ presentatie.py
@@ -1,2 +1,9 @@
+from helper import onderstreep
+
 def presenteer(mijn_dictionary,totaal):
-       pass
+    for item, waarde in mijn_dictionary.items():
+        print(f"{item} : {waarde} euro")
+
+    totaal_regel = f"Totaal : {totaal} euro"
+    print("=" * len(totaal_regel))
+    print(totaal_regel)
